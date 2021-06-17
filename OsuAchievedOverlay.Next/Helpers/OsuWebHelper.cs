cc32ce7773b3df57872559eb9b1f8348b510036f
Mark Meijer <engineermarkpro@gmail.com>
2021-06-17 07:55:09 +0200

    Updated ILMerge nuget package

---
+++ OsuAchievedOverlay.Next/Helpers/OsuWebHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuAchievedOverlay.Next.Helpers
+{
+    public class OsuWebHelper
+    {
+    }
+}
